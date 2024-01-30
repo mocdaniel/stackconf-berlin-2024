cc6a9c58d077fe6d87f54aa3bcba89faa31e36f0
Daniel Bodky <daniel.bodky@netways.de>
2024-01-30 16:39:12 +0100

    Adds global Header

---
+++ global-top.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <div class="absolute top-0 left-0 right-0 flex flex-row  justify-end p-4">
+      <img class="w-36" src="/logo.svg" alt="logo" />
+    </div>
+  </header>
+</template>
